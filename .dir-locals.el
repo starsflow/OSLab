82eed5ca32fe5b295980189ff2c5217d501a8fb2
ubuntu <ubuntu@localhost.localdomain>
2023-07-08 14:22:48 +0800

    first commit

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,12 @@
-((c-mode
-  (indent-tabs-mode . nil)
+((nil
+  (indent-tabs-mode . t)
+  (tab-width . 8))
+ (c-mode
   (c-file-style . "bsd")
-  (c-basic-offset . 2)))
+  (c-basic-offset . 8))
+ (shell-mode
+  (sh-basic-offset . 8)
+  (sh-indentation . 8))
+ (python-mode
+  (indent-tabs-mode . nil))
+ )
